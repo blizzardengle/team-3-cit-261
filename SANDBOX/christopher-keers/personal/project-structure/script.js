9f17889d80c65b52e354760ccdbdbfac23fff4a6
Jesus Arredondo <chuyarredondo@gmail.com>
2016-06-25 21:43:51 -0500

    add local storage functions

--- SANDBOX/christopher-keers/personal/project-structure/script.js
+++ SANDBOX/christopher-keers/personal/project-structure/script.js
@@ -1,3 +1,4 @@
+
 function collection(name){
 	this.name = name;
 	this.pointer = null;
@@ -6,6 +7,9 @@ function collection(name){
 
 collection.prototype.add = function(name){
 	var newRecord = new collectionRecord(name);
+	var testJson = {"name":"jesus","last":"arredondo"};
+	
+	var canAdd = addToLocalStorage(name,testJson);
 	
 	/**
 	 * ATTEMPT TO CREATE A LOCAL STORAGE FILE WITH THIS NEW ID 
@@ -14,7 +18,7 @@ collection.prototype.add = function(name){
 	// Pass newRecord.filename as a paramter and should get TRUE or FALSE back
 	// Change if statement below to account use the returned result
 	
-	if (true===true){
+	if (canAdd===true){
 		if(this.pointer===null){
 			this.pointer = newRecord;
 			this.length += 1;
@@ -67,7 +71,7 @@ collection.prototype.deserialize = function(){
 	 */
 	
 	// Pass filename as a paramter and should get OBJECT STRING or FALSE back
-	var result = "RETURNED OBJECT STRING HERE";
+	var result =searchLocalStorage(filename);
 	
 	if(result!==false){
 		var current = JSON.parse(result).pointer;
@@ -89,12 +93,14 @@ function collectionRecord(name){
 	this.previous = null;
 }
 
-collectionRecord.prototype.remove = function(){
+collectionRecord.prototype.remove = function(filename){
 	/**
 	 * CALL FUCTION THAT REMOVES A FILE FROM LOCAL STORAGE
 	 */
 	
 	// Pass in the filename parameter
+
+	removeLocalStorage(filename);
 	
 	/**
 	 * Empty out this object (manual garbage collection)
@@ -107,4 +113,101 @@ collectionRecord.prototype.remove = function(){
 
 function generateId(){
 	return String(new Date(new Date().getTime())).replace(/[^a-zA-Z0-9]+/g,"");
+}
+
+
+
+/*These are the funcitons created by Jesus Arredondo for local storage*/
+
+/*Input: fileName and a JSON Object 
+  Process: The funvtion checks if error 22 related to not having local storage space is triggered if not then it adds the record 
+  and let's assign true meaning there is speace to add the record in local storage
+  Output true or false if the file or record is already found on local storage
+*/
+
+ function addToLocalStorage(fileName,Obj){
+	var canAdd = true;
+	try {
+		var exists = false;
+		exists=existsLocalStorage(fileName);
+		if (exists===true){
+			return
+		}else{
+			localStorage.setItem(fileName, JSON.stringify(Obj));	
+		}
+	} catch(e) {
+  		if (isQuotaExceeded(e)) {
+    		// Storage full, maybe notify user or do some clean-up
+    		canAdd=false;
+  		}
+	}
+	return canAdd;
+}
+
+/* Input: Local Storage space exception
+   Process: It checks for different error codes and names depending on the browser
+   Output: True if there is no space on local storage false if there is space to save the JSON object
+*/
+function isQuotaExceeded(e) {
+  var quotaExceeded = false;
+  if (e) {
+    if (e.code) {
+      switch (e.code) {
+        case 22:
+          quotaExceeded = true;
+          break;
+        case 1014:
+          // Firefox
+          if (e.name === 'NS_ERROR_DOM_QUOTA_REACHED') {
+            quotaExceeded = true;
+          }
+          break;
+      }
+    } else if (e.number === -2147024882) {
+      // Internet Explorer 8
+      quotaExceeded = true;
+    }
+  }
+  return quotaExceeded;
+}
+
+/* Input fileName: Key of the record or file in local storage that needs to be seach for
+   Output: True if the file or record is found and false if the record is not found on local storage
+   Process: Checks if the getItem method of local storage throws something different to null
+*/
+function existsLocalStorage(fileName){
+	var exists = false;
+
+	
+	if(localStorage.getItem(fileName)!==null){
+		exists= true;
+		return exists;
+	}
+	return exists;
+}
+
+
+/* Input fileName: Key of the record or file in local storage that needs to be seach for
+   Output:Returns the JSON object if the file or record is found, and false if the record is not found on local storage
+   Process: Checks if the getItem method of local storage throws something different to null
+*/
+
+function searchLocalStorage(fileName){
+	var foundItem = "";
+	var exists = false;
+
+	if(localStorage.getItem(fileName)!==null){
+		foundItem = JSON.parse(localStorage.getItem(localStorage.key(i));
+		return foundItem;
+	}
+
+	return exists;
+}
+
+/*Input: file name or record key for the record to be deleted from local storage*/
+
+function removeLocalStorage(fileName){
+	if (localStorage.getItem(fileName)!==null){
+		localStorage.removeItem(fileName);
+	}
 }
