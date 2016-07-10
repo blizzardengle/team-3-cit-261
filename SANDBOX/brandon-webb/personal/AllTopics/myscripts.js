1bf5a8e6e449a809f576625a7a0bb71c7aa092eb
Blaxican <brandonmwebb@gmail.com>
2016-07-09 21:43:47 -0500

    BMW Changes

--- SANDBOX/brandon-webb/personal/AllTopics/myscripts.js
+++ SANDBOX/brandon-webb/personal/AllTopics/myscripts.js
@@ -5,34 +5,35 @@ function checkStorage() {
     if (typeof(Storage) !== "undefined") {
     document.getElementById("storage_status").innerHTML = "Storage is working";
     } else {
-  //Sorry! No Web Storage support..
+    //Sorry! No Web Storage support..
     }
 }
 
 function getUserList() {
-        var filename = "objcts.txt";
+        var filename = "objects.txt";
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
-                if(xhttp.readyState == 4 && xhttp.status == 200) {	//file exists
-                        createUserList(xhttp.responseText);
-                } else { //should be a 404
-                        if(xhttp.status != 200 && xhttp.readyState != 1)	//only display if real error occurs
-                                document.getElementById("details").innerHTML = "State: " + xhttp.readyState + ", Status: " + xhttp.status;
-                }
-        }
+            if(xhttp.readyState === 4 && xhttp.status === 200) {	//file exists
+                    createUserList(xhttp.responseText);
+            } else { //should be a 404
+                    if(xhttp.status !== 200 && xhttp.readyState !== 1)	//only display if real error occurs
+                            document.getElementById("details").innerHTML = "State: " + xhttp.readyState + ", Status: " + xhttp.status;
+            }
+        };
         xhttp.open("GET", filename, true);
         xhttp.send();
 }
 	
 function createUserList(users) {
-        users = JSON.parse(users);	//convert to JSON
+        //users = JSON.parse(users);
+       
 
         for(var i = 0; i < users.length; i++) {	//loop through the list
-                var option = document.createElement("option");
-                option.value = users[i];
-                var text = document.createTextNode(users[i]);
-                option.appendChild(text);
-                document.getElementById("users").appendChild(option);
+            var option = document.createElement("option");
+            option.value = users[i];
+            var text = document.createTextNode(users[i]);
+            option.appendChild(text);
+            document.getElementById("users").appendChild(option);
         }
 }
 	
@@ -42,12 +43,12 @@ function getUserData() {
         var filename = "ajax-json-data.php?action=fetch&name=" + user;
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
-                if(xhttp.readyState == 4 && xhttp.status == 200) {	//file exists
+                if(xhttp.readyState === 4 && xhttp.status === 200) {	//file exists
                         displayUserData(xhttp.responseText);
                 } else { //processing
                         document.getElementById("details").innerHTML = "Fetching the data, please wait a second.";
                 }
-        }
+        };
         xhttp.open("GET", filename, true);
         xhttp.send();
 }
@@ -61,10 +62,10 @@ function displayUserData(data) {
                 orders += "Quantity: " + data[i].Quantity + "<br/>";
                 orders += "Item Price: " + data[i].Price + "<br/>";
                 orders += "Total Price: " + data[i].Total + "<br/>";
-                if(data.length-1 != i) orders += "<hr/>";
+                if(data.length-1 !== i) orders += "<hr/>";
         }
 
-        if(orders == "") orders = "That user currently has no orders."
+        if(orders === "") orders = "That user currently has no orders."
 
         document.getElementById("details").innerHTML = orders;
 }
